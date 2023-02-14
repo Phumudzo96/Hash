2970aa15037ed9f5fbd70613504ce5f57df336b9
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-15 01:04:34 +0200

    Add files via upload

---
+++ hash.py
@@ -0,0 +1,8 @@
+countyMaps = {"South Africa" : "Cape Town",                   # create a dictionary with coutries and their captial cities
+            "Sweden" : "Stockhlom",
+            "England" : "London",
+            " USA" : "New York",
+            "Nigeria" : "Lagos",
+            "Egypt" : "Cairo",
+            "Australia" : "Sydney"}
+print(countyMaps["Sweden"])                                    # Whenever the the country is called the capital city is displayed
